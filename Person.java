b45a23a1f5101b3c23c2cd45155839965eb524dc
Likhithalv333 <133503046+Likhithalv333@users.noreply.github.com>
2024-02-07 15:21:56 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,20 @@
+package com.sathya.collectinslabqestions;
+
+public class Person {
+	String items;
+
+	public Person(String items) {
+		super();
+		this.items = items;
+	}
+
+	public String getItems() {
+		return items;
+	}
+
+	public void setItems(String items) {
+		this.items = items;
+	}
+	
+
+}
